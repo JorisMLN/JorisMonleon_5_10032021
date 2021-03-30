9ed6be1e6d79528e7353d7d49f53268e22178e85
JorisMLN <monleon.joris@gmail.com>
2021-03-30 19:06:11 +0200

    Panier 2.0 fonctionnel (sans incrémentation)

--- Js/teddy.js
+++ Js/teddy.js
@@ -5,7 +5,7 @@
 /* Récupération des paramètres dans l'URL */ /* exemple: ?id=5be9c8541c9d440000665243 */
 let teddyParameters = window.location.search;
 let teddyApiParameters = teddyParameters.substr(4);
-console.log(teddyApiParameters); 
+console.log(teddyApiParameters);
 
 let requestTeddy = new XMLHttpRequest();
 requestTeddy.onreadystatechange = function(){
@@ -14,35 +14,50 @@ requestTeddy.onreadystatechange = function(){
         console.log(teddy);
 
         /* HTML dynamique pour les pages Teddy en details */
-        htmlString = `
-        <article class="teddy__page">
-            <div class="teddy__page--left">
-                <img src="${teddy.imageUrl}" alt="ours en peluche">
-            </div>
-            <div class="teddy__page--right">
-                <div class="teddy__page--right--nameID">
-                    <h1>${teddy.name}</h1>
-                    <h2>ID: ${teddy._id}</h2>
-                </div>
-            <div class="teddy__page--right--description">
-                <p> ${teddy.description} </p>
-            </div>
-            <div class="teddy__page--right--bottom">
-                <div>-
-                    <label> Couleur </label>
-                    <select name="couleur" id="teddyColors">
-                
-                    </select>
-                </div>
-                <a class="button" id='buttonTeddy'>Ajouter au Panier</a>
-            </div>
-        </article>
-        `
+        htmlString = getArticleTemplate(teddy);
+
         /* Variable de la Div qui accueille le details */
         let teddyPage =  document.getElementById('teddyPage');
         teddyPage.innerHTML = htmlString;
 
         /* Boucle pour les couleurs des Teddies */
+        setColorsOptions(teddy);
+
+        /* Ecoute du click d'ajout au panier */
+        addTeddyToCart(teddy);
+    }
+}
+requestTeddy.open("GET", `http://localhost:3000/api/teddies/${teddyApiParameters}`);
+requestTeddy.send();
+
+function getArticleTemplate(teddy){
+    return `<article class="teddy__page">
+                <div class="teddy__page--left">
+                    <img src="${teddy.imageUrl}" alt="ours en peluche">
+                </div>
+                <div class="teddy__page--right">
+                    <div class="teddy__page--right--nameID">
+                        <h1>${teddy.name}</h1>
+                        <h2>ID: ${teddy._id}</h2>
+                    </div>
+                <div class="teddy__page--right--description">
+                    <p> ${teddy.description} </p>
+                </div>
+                <div class="teddy__page--right--bottom">
+                    <div>-
+                        <label> Couleur </label>
+                        <select name="couleur" id="teddyColors">
+                    
+                        </select>
+                    </div>
+                    <a class="button" id="buttonTeddy">Ajouter au Panier</a>
+                </div>
+            </article>
+        `;
+}
+
+function setColorsOptions(teddy){
+    console.log(teddy.colors);
         let colorString = '';
         teddy.colors.forEach((colors) => {
                 colorString += `
@@ -50,42 +65,45 @@ requestTeddy.onreadystatechange = function(){
             `;
             });
         let teddyColors = document.getElementById('teddyColors'); 
-        teddyColors.innerHTML = colorString; 
-    }
+        teddyColors.innerHTML = colorString;
 }
-requestTeddy.open("GET", `http://localhost:3000/api/teddies/${teddyApiParameters}`);
-requestTeddy.send();
 
-
+function addTeddyToCart(teddy){
+    let btnToCommand = document.getElementById('buttonTeddy');
+    btnToCommand.addEventListener('click', function(){
+        console.log(sessionStorage);
+        cart(teddy);
+    });
+}
 
 /* ---------------- P A N I E R ---------------- */
 
-/* Ecoute du click d'ajout au panier */
-let btnToCommand = document.getElementById('buttonTeddy');
-btnToCommand.addEventListener('click', function(){
-    let teddyCommand_json = sessionStorage.getItem("teddyCommand");
-    console.log(teddyCommand_json);
-    cart();
-});
 
 /* Function de gestion du panier pour le sessionStorage */
-function cart() {
-    if (teddyCommand == false){
-        let panier = new Panier ('John Doe', []);
-        panier.addItem(this.teddy);
-        teddyCommand_json = sessionStorage.setItem("teddyCommand");
+function cart(teddy) {
+    let panier;
+    if (sessionStorage.teddies === undefined || sessionStorage.teddies === null){ /*(!sessionsStorage.teddies)*/
+        panier = new Panier ('John Doe', []);
     } else {
-        cartAlready();
+        let panier_json = sessionStorage.getItem("teddies");
+        panier = Object.assign(new Panier, JSON.parse(panier_json));
     }
+    panier.addItem(teddy);
+    sessionStorage.setItem("teddies", JSON.stringify(panier));
+    console.log(sessionStorage);
 };
 
-function cartAlready() {
-    if (this.teddy == false){
+
+function cartAlready(teddy) {
+    let panier_json = sessionStorage.getItem("teddies");
+    if (teddy == false){
         this.panier.addItem(this.teddy);
     } else {
         this.teddy ++;
     }
 };
+
+
 
 /* Class pour le panier */
 class Panier {
