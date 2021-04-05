badb5d50d3ef04baa3421db0940d066007089c25
JorisMLN <monleon.joris@gmail.com>
2021-04-05 17:55:36 +0200

    MaJ Panier + Recall API

--- Js/panier.js
+++ Js/panier.js
@@ -3,94 +3,74 @@ import Panier from './panierClass.js';
 /* ---------------- P A G E - P A N I E R ---------------- */
 
 
-
-/* Récupération des données du panier dans le sessionStorage */
 let panier_json = sessionStorage.getItem("panier");
+console.log(panier_json);
 let panier = Object.assign(new Panier, JSON.parse(panier_json));
 console.log(panier);
 
-/* HtmlString + Boucle ForEach pour créer dynamiquement le récapitulatif de commande */
-generatePanier();
+generatePanier(); /* HtmlString + Boucle ForEach pour créer dynamiquement le récapitulatif de commande */
+
 
 function generatePanier(){
+
     let htmlString = '';
-    let totalPrice = 0;
+    // let totalPrice = 0;
     for(let teddyId in panier.teddies){
+
+        let requestTeddies = new XMLHttpRequest();
+        requestTeddies.onreadystatechange = function(){
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200){
+                let teddyResponse = JSON.parse(this.responseText);
+                console.log(teddyResponse);
+
+                htmlString += getPanierTemplate(teddyResponse);
+                let teddyListCommand = document.getElementById('recapPanier'); /* Variable de la Div qui accueille le récapitulatif */
+                teddyListCommand.innerHTML = htmlString;
+            }
+        };
+        requestTeddies.open("GET", `http://localhost:3000/api/teddies/${[teddyId]}`);
+        requestTeddies.send();
+
         htmlString += getPanierTemplate(teddyId);
         // totalPrice += teddy.price;
     };
-    /* Variable de la Div qui accueille le récapitulatif */
-    let teddyListCommand = document.getElementById('recapPanier'); 
+    let teddyListCommand = document.getElementById('recapPanier'); /* Variable de la Div qui accueille le récapitulatif */
     teddyListCommand.innerHTML = htmlString;
+    // let priceCommand = document.getElementById("priceCommand"); /* Affichage du total Price dans le HTML */
+    // priceCommand.innerHTML = `Total de la commande: ${totalPrice} €`;
 
-    /* Affichage du total Price dans le HTML */
-    let priceCommand = document.getElementById("priceCommand");
-    priceCommand.innerHTML = `Total de la commande: ${totalPrice} €`;
-
-    bindRemoveTeddy();
+    bindRemoveTeddy(); /* Button pour Remove un article du panier */
 }
-
 
 function getPanierTemplate(teddyId){
-    // return `<div class="recapPanier__teddy">
-    //             <div class="recapPanier__teddy__left">
-    //                 <img src="${teddy.imageUrl}" alt="ours en peluche">
-    //             </div>
-
-    //             <div class="recapPanier__teddy__mid">
-    //                 <div class="title">
-    //                     <div class="title__name">
-    //                         ${teddy.name}
-    //                     </div>
-    //                     <div class="incrementation">
-    //                     x1
-    //                     </div>
-    //                     <div class="title__id">
-    //                         ${teddyId}
-    //                     </div>
-    //                 </div>
-    //             </div>
-
-    //             <div class="recapPanier__teddy__right">
-    //                 <div class="recapPanier__right__price">
-    //                     ${teddy.price}
-    //                 </div>
-    //                 <a class="teddyRemove" id="${teddy._id}"> X </a>
-    //             </div>
-
-    //         </div>
-    //     `;
     return `<div class="recapPanier__teddy">
-    <div class="recapPanier__teddy__left">
-        <img src="" alt="ours en peluche">
-    </div>
+                <div class="recapPanier__teddy__left">
+                    <img src="" alt="ours en peluche">
+                </div>
 
-    <div class="recapPanier__teddy__mid">
-        <div class="title">
-            <div class="title__name">
-            
-            </div>
-            <div class="incrementation">
-            ${panier.teddies[teddyId]}
-            </div>
-            <div class="title__id">
-                ${teddyId}
-            </div>
-        </div>
-    </div>
+                <div class="recapPanier__teddy__mid">
+                    <div class="title">
+                        <div class="title__name">
+                        
+                        </div>
+                        <div class="incrementation">
+                            ${panier.teddies[teddyId]}
+                        </div>
+                        <div class="title__id">
+                            ${teddyId}
+                        </div>
+                    </div>
+                </div>
 
-    <div class="recapPanier__teddy__right">
-        <div class="recapPanier__right__price">
-            
-        </div>
-        <a class="teddyRemove" id="${teddyId}"> X </a>
-    </div>
-
-</div>
-`;
+                <div class="recapPanier__teddy__right">
+                    <div class="recapPanier__right__price">
+                        
+                    </div>
+                    <a class="teddyRemove" id="${teddyId}"> X </a>
+                </div>
+            </div>
+            `;
 }
-
-/* Button pour Remove un article du panier */
 function bindRemoveTeddy(){
     let btnTeddyRemove = document.getElementsByClassName("teddyRemove");
 
@@ -109,3 +89,62 @@ function bindRemoveTeddy(){
 
 
 
+
+/* ---------------- 2 N D E - W A Y ---------------- */
+
+// function getPanierTemplate(teddyResponse){
+//     return `<div class="recapPanier__teddy">
+//                 <div class="recapPanier__teddy__left">
+//                     <img src="${teddyResponse.imageUrl}" alt="ours en peluche">
+//                 </div>
+
+//                 <div class="recapPanier__teddy__mid">
+//                     <div class="title">
+//                         <div class="title__name">
+//                         ${teddyResponse.name}
+//                         </div>
+//                         <div class="title__id">
+//                             ${teddyResponse._id}
+//                         </div>
+//                     </div>
+//                 </div>
+
+//                 <div class="recapPanier__teddy__right">
+//                     <div class="recapPanier__right__price">
+                        
+//                     </div>
+//                     <a class="teddyRemove" id="${teddyResponse._id}"> X </a>
+//                 </div>
+//             </div>
+//             `;
+// }
+
+// function bindRemoveTeddy(){
+//     let btnTeddyRemove = document.getElementsById(`${teddyResponse._id}`);
+//     btnTeddyRemove.addEventListener('click', function(){
+//         panier.removeItem(`${teddyResponse._id}`);
+//         sessionStorage.setItem("panier", JSON.stringify(panier));
+//         console.log(JSON.parse(sessionStorage.panier));
+//         generatePanier();
+//     });
+// }
+
+
+
+/* ---------------- S O L O - D A T A - A P Y ---------------- */
+
+// requestPanierAPI(); /* Requete WEB pour avoir les infos des teddies commandés*/
+// function requestPanierAPI(){
+
+//     for(let teddyId in panier.teddies){
+//         let requestTeddies = new XMLHttpRequest();
+//         requestTeddies.onreadystatechange = function(){
+//             if (this.readyState == XMLHttpRequest.DONE && this.status == 200){
+//                 let response = JSON.parse(this.responseText);
+//                 console.log(response);   
+//             }
+//         };
+//         requestTeddies.open("GET", `http://localhost:3000/api/teddies/${[teddyId]}`);
+//         requestTeddies.send();
+//     }
+// }
--- Js/panierClass.js
+++ Js/panierClass.js
@@ -1,3 +1,7 @@
+
+/* ---------------- C L A S S - P A N I E R ---------------- */
+
+
 export default class Panier {
     constructor(owner, teddies){
         this.owner = owner;
@@ -18,3 +22,27 @@ export default class Panier {
         }
     }
 }
+
+
+/* ---------------- 2 D E - P A N I E R ---------------- */
+
+// export default class Panier {
+//     constructor(owner, teddies){
+//         this.owner = owner;
+//         this.teddies = teddies;
+//     }
+//     addItem(teddyId){
+//         if(!this.teddies[teddyId]){
+//             this.teddies[teddyId] = 1;
+//         } else {
+//             this.teddies[teddyId] += 1;
+//         }
+//         // this.teddies[teddy._id] = !this.teddies[teddy._id] ? 1 : this.teddies[teddy._id] + 1;
+//     }
+//     removeItem(teddyId){
+//         this.teddies[teddyId] -= 1;
+//         if(this.teddies[teddyId] === 0){
+//             delete this.teddies[teddyId];
+//         }
+//     }
+// }
